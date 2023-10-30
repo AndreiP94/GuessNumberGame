e8732214784e7d14649a837ccbf250b09b6500fb
Andrei Pop <149314486+AndreiP94@users.noreply.github.com>
2023-10-30 11:08:49 +0200

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -12,24 +13,14 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = number;
 
-    if(score > highscore){
+    if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
-      
-  } else if (guess > number) {
-    if (score > 1) {
-      document.querySelector('.message').textContent =
-        ' Numarul este prea mare!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = ' Ai pierdut jocu!';
-      document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < number) {
+  } else if (guess != number) {
     if (score > 1) {
       document.querySelector('.message').textContent =
-        ' Numarul este prea mic!';
+        guess > number ? ' Numarul este prea mare!' : 'Numarul este mai mic!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
